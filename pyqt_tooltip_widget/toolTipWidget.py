a05dbf105b6b48d4c5dad40baf0223c89772a263
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-06 15:19:45 +0900

    Add new functions, refactoring

--- pyqt_tooltip_widget/toolTipWidget.py
+++ pyqt_tooltip_widget/toolTipWidget.py
@@ -9,6 +9,7 @@ class ToolTipWidget(QWidget):
         self.__initUi()
 
     def __initVal(self, widget_to_set_tooltip_widget):
+        self.__stillOpenWhenCursorLeaveFromTooltipWidget = False
         self.__tooltip_widget_name = 'tooltipWidget'
 
         self.__widget_to_set_tooltip_widget = widget_to_set_tooltip_widget
@@ -20,15 +21,34 @@ class ToolTipWidget(QWidget):
         self.__widget_to_set_tooltip_widget.setMouseTracking(True)
         self.__widget_to_set_tooltip_widget.installEventFilter(self)
 
+    def isStillOpenWhenCursorLeaveFromTooltipWidget(self) -> bool:
+        return self.__stillOpenWhenCursorLeaveFromTooltipWidget
+
+    def setStillOpenWhenCursorLeaveFromTooltipWidget(self, f: bool):
+        self.__stillOpenWhenCursorLeaveFromTooltipWidget = f
+
     def eventFilter(self, obj, e):
-        if obj.objectName() == self.__widget_name:
-            if e.type() == 10:
-                self.__show()
-            elif e.type() == 11:
-                self.close()
-            elif e.type() == 2:
-                self.close()
+        if self.isStillOpenWhenCursorLeaveFromTooltipWidget():
+            if obj.objectName() == self.__tooltip_widget_name or obj.objectName() == self.__widget_name:
+                if obj.objectName() == self.__widget_name:
+                    self.__execToolTip(e)
+                else:
+                    if e.type() == 10:
+                        self.__show()
+                    elif e.type() == 11:
+                        self.close()
+        else:
+            if obj.objectName() == self.__widget_name:
+                self.__execToolTip(e)
         return super().eventFilter(obj, e)
+
+    def __execToolTip(self, e):
+        if e.type() == 10:
+            self.__show()
+        elif e.type() == 11:
+            self.close()
+        elif e.type() == 2:
+            self.close()
 
     def __show(self):
         p = self.__widget_to_set_tooltip_widget.mapToGlobal(
