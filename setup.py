189cd80723eb4583d98fcd5cd395832e6ef5f256
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 07:25:00 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-tooltip-widget',
@@ -9,6 +16,8 @@ setup(
     packages=find_packages(),
     description='PyQt QWidget as a tooltip',
     url='https://github.com/yjg30737/pyqt-tooltip-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
